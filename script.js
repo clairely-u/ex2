be93a0454695799ecff2db4cef999ce010045b7f
Claire Utrecht <utrechtc@gmail.com>
2022-01-24 11:19:21 -0500

    first commit ex2

---
+++ script.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var x = document.getElementById("myLinks"); // Put this
+    if (x.style.display === "block") {
+      x.style.display = "none"; //and this at the bottom of both functions
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function numbersShow() {
+    const num = document.querySelector("#numbers");
+    num.style.display = "block";
+    
+    const letter = document.querySelector("#letters");
+    letter.style.display = "none";
+  };
+
+  function lettersShow() {
+      const letter = document.querySelector("#letters");
+      letter.style.display = "block";
+
+      const num = document.querySelector("#numbers");
+      num.style.display = "none";
+  };
